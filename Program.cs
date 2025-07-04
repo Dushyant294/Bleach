cc7112fe3ff589e6d3e383c983307a72113d0946
yd999 <yd999@DUSHYANT>
2025-07-04 10:08:11 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Bleach
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello  world!");
+            Console.Read();
+        }
+    }
+}
