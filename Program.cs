7a850512345bbd26ab1ddefcb3f4d180dfd6901a
Dushyant294 <yd.999017@gmail.com>
2025-07-11 10:20:31 +0530

    11/07/2025

--- Program.cs
+++ Program.cs
@@ -1,13 +1,35 @@
 ﻿using System;
+using System.ComponentModel;
 
-namespace Bleach
+namespace BD
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hello  world!");
+            //Console.WriteLine("hello  world!");
+            //Console.WriteLine("enter no 1 :");
+            //int a = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("enter no 2 :");
+            //int b = Convert.ToInt32(Console.ReadLine());
+            //int c = a+b;
+            //Console.WriteLine("addtion of the above two no. are :" + c);
+
+            //Console.WriteLine("Name: Dushyant Dangar");
+            //Console.WriteLine("DOB: 11/03/2005");
+            //Console.WriteLine("Address: 54 , Narayan Nagar");
+            //Console.WriteLine("         Kalawad Road");
+            //Console.WriteLine("City: Rajkot");
+            //Console.WriteLine("Pincode: 360 005");
+            //Console.WriteLine("Country: India");
+            //Console.WriteLine("Email: yd.999017@gmail.com");
+
+            
+
             Console.Read();
+
+
         }
     }
+
 }
