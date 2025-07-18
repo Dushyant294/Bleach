b75033687bd670f1fd370ddc4fcf824be35811d4
Dushyant294 <yd.999017@gmail.com>
2025-07-18 10:58:54 +0530

    18/7/2025

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel;
+using System.Linq;
 
 namespace BD
 {
@@ -24,7 +26,92 @@ namespace BD
             //Console.WriteLine("Country: India");
             //Console.WriteLine("Email: yd.999017@gmail.com");
 
-            
+            //tut 1 21
+            //Console.Write("Input the number of terms: ");
+            //int n = int.Parse(Console.ReadLine());
+
+            //Console.Write("Input number: ");
+            //int num = int.Parse(Console.ReadLine());
+
+            //int term = 0;
+            //int sum = 0;
+
+            //Console.WriteLine();
+            //Console.Write("Series: ");
+
+            //for (int i = 1; i <= n; i++)
+            //{
+            //    term = term * 10 + num;
+            //    sum += term;
+
+            //    Console.Write(term);
+            //    if (i < n)
+            //        Console.Write(" + ");
+            //}
+
+            //Console.WriteLine();
+            //Console.WriteLine("The Sum is : " + sum);
+
+
+            //tut 1 14
+            //Console.Write("Enter a number: ");
+            //int num = Convert.ToInt32(Console.ReadLine());
+            //int reverse = 0;
+            //int temp = num;
+
+            //while (temp > 0)
+            //{
+            //    reverse = reverse * 10 + temp % 10;
+            //    temp /= 10;
+            //}
+            //while (reverse > 0)
+            //{
+            //    int digit = reverse % 10;
+
+            //    if (digit == 0) Console.Write("Zero ");
+            //    else if (digit == 1) Console.Write("One ");
+            //    else if (digit == 2) Console.Write("Two ");
+            //    else if (digit == 3) Console.Write("Three ");
+            //    else if (digit == 4) Console.Write("Four ");
+            //    else if (digit == 5) Console.Write("Five ");
+            //    else if (digit == 6) Console.Write("Six ");
+            //    else if (digit == 7) Console.Write("Seven ");
+            //    else if (digit == 8) Console.Write("Eight ");
+            //    else if (digit == 9) Console.Write("Nine ");
+            //    reverse /= 10;
+            //}
+
+            //Console.WriteLine();
+
+            //tut 1 10 write a program which accepts mobile no. as a string from the user and converts the last 5 digits into x. input 1234567890 output 12345xxxxx 
+
+            // 
+
+            //Console.Write("Enter a number: ");
+            //int n = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine( n + " " + n +" " + n );
+            //for (int i = 1; i <= n; i++)
+            //{
+            //    Console.WriteLine(n + "" + i + " " + n);
+            //}
+
+            //*
+            //# *
+            //* # * 
+            //# * # *
+            //special patter paresh tanna sir 
+
+            //array built in functions
+            //int[] a = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            //Console.WriteLine("Array Length: " + a.Length);
+            //Console.WriteLine("Array Max: " + a.Max());
+            //Console.WriteLine("Array Min: " + a.Min());
+            //Console.WriteLine("Array Sum: " + a.Sum());
+            //Console.WriteLine("Array Average: " + a.Average());
+            //Console.WriteLine("Array Reverse: " + string.Join(", ", a.Reverse()));
+
+
+
 
             Console.Read();
 
